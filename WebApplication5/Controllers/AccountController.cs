f41fadad99020d353330cd82827b3cbc525bcefa
luis <albert.malorca@gmail.com>
2017-05-30 12:45:20 -0500

    cambio12

--- WebApplication5/Controllers/AccountController.cs
+++ WebApplication5/Controllers/AccountController.cs
@@ -16,7 +16,7 @@ namespace WebApplication5.Controllers
     public class AccountController : Controller
     {
 
-        //ASD11ASDASD
+        //ASD11ASDASD11212
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
